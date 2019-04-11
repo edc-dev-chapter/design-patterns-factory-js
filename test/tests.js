83d5df97533c8aa642b9954360ae9fcb1977f54e
Jerzy Bielicki <jerzy.bielicki@ge.com>
2019-04-11 07:35:17 +0200

    Update tests and README.md

--- test/tests.js
+++ test/tests.js
@@ -21,6 +21,27 @@ describe('Tests', () => {
     });
 
     it('veggie pizza should contain no "pepperoni"', () => {
+        const veggiePizza = new VeggiePizza();
+        veggiePizza.prepare();
+        const ingredients = veggiePizza.getIngredients();
+        expect(ingredients.length >= 2 && !ingredients.includes('pepperoni')).to.be.true;
+    });
+
+    it('cheese pizza should contain "double cheese"', () => {
+        const cheesePizza = new CheesePizza();
+        cheesePizza.prepare();
+        const ingredients = cheesePizza.getIngredients();
+        expect(ingredients.length >= 2 && ingredients.includes('double cheese')).to.be.true;
+    });
+
+    it('pepperoni pizza should contain "pepperoni"', () => {
+        const pepperoniPizza = new PepperoniPizza();
+        pepperoniPizza.prepare();
+        const ingredients = pepperoniPizza.getIngredients();
+        expect(ingredients.length >= 2 && ingredients.includes('pepperoni')).to.be.true;
+    });
+
+    it('Factory made veggie pizza should contain no "pepperoni"', () => {
         const pizzaFactory = new PizzaFactory();
         const pizza = pizzaFactory.createPizza('veggie');
         pizza.prepare();
@@ -28,7 +49,7 @@ describe('Tests', () => {
         expect(ingredients.length >= 2 && !ingredients.includes('pepperoni')).to.be.true;
     });
 
-    it('cheese pizza should contain "double cheese"', () => {
+    it('Factory made cheese pizza should contain "double cheese"', () => {
         const pizzaFactory = new PizzaFactory();
         const pizza = pizzaFactory.createPizza('cheese');
         pizza.prepare();
@@ -36,7 +57,7 @@ describe('Tests', () => {
         expect(ingredients.length >= 2 && ingredients.includes('double cheese')).to.be.true;
     });
 
-    it('pepperoni pizza should contain "pepperoni"', () => {
+    it('Factory made pepperoni pizza should contain "pepperoni"', () => {
         const pizzaFactory = new PizzaFactory();
         const pizza = pizzaFactory.createPizza('pepperoni');
         pizza.prepare();
